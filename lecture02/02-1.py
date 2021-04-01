6a8dc7c348a6ed51ce178007c6c3d2a9fe00a31e
devtory-lab <an44andedu@gmail.com>
2021-04-01 12:49:59 +0900

    first commit

---
+++ lecture02/02-1.py
@@ -0,0 +1,9 @@
+a = 2
+print(a)
+
+b = 0xABC
+print(b)
+
+#print(a ** b)
+
+print(7 % 3)
---
+++ lecture02/02-2.py
@@ -0,0 +1,37 @@
+food = "Python's favorite food is perl"
+print(food)
+
+say = "\"Python is very easy.\" he says."
+print(say)
+
+multiline = "Life is too short\nYou need python"
+print(multiline)
+
+multiline='''
+    Life is too short2 \' \" \\
+    You need python1\000\000ttt
+    '''
+
+print(multiline)
+
+head = "Python"
+tail = " is fun!"
+print(head + tail)
+
+a = "python"
+print(a * 2)
+
+print("=" * 50)
+print("My Program")
+print("=" * 50)
+
+a = "Life is too short"
+print(len(a))
+
+a = "Life is too short, You need Python"
+print(a[0], a[12], a[-1])
+print(a[0] + a[1] + a[2] + a[3])        #Life
+print(a[0:4])                           #Life
+print(a[19:])       #You need Python
+
+
