93101afaaa4c7c737bd6f7731e9ebf448b2d08a1
devtory-lab <an44andedu@gmail.com>
2021-04-05 11:18:37 +0900

    lecture05-2 모듈

---
+++ lecture05/05-2.py
@@ -0,0 +1,19 @@
+import mod1
+
+print(mod1.add(1, 2))
+print(mod1.sub(2, 1))
+print("==============")
+
+from mod1 import add, sub
+print(add(3, 5))
+print(sub(6, 5))
+print("==============")
+
+
+print("==============")
+
+
+print("==============")
+
+
+print("==============")
